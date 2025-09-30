2c86d68a3e5910930eaf6943fcc0f0ac533c0fb3
davi <jorgedavi2709@gmail.com>
2025-09-30 17:41:03 -0300

    Style (home): Aplicamos o header e modificado cor de fundo para destaque do Header.

--- app/home.tsx
+++ app/home.tsx
@@ -1,17 +1,46 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Link, router } from "expo-router";
+import { Text, View, SafeAreaView } from "react-native";
+import Header from "./components/header"; 
 
 const Home = () => {
-    return (
-        <View className="flex-1 bg-medix-100 items-center justify-center px-6 gap-8">
-            <Text className="text-4xl font-bold text-medix-600 mb-2">👋 Olá!</Text>
-            <Text className="text-lg text-medix-700 mb-1">Você está logado com sucesso.</Text>
-            <Text className="text-sm text-medix-800 mb-8">Aproveite sua experiência no app 💚</Text>
 
-            <Link replace href="/" className="bg-medix-600 px-6 py-3 rounded-lg">
-                <Text className="text-white text-base font-semibold">Sair</Text>
-            </Link>
-        </View>
+    // ícone da esquerda (menu)
+    const handleOpenDrawer = () => {
+        console.log('Ação para abrir o menu lateral (Drawer Navigation)');
+        
+    };
+
+    
+    const handleNotifications = () => {
+        console.log('Navegar para a tela de notificações');
+        // usando o exporouter, vamos alterar o caminho para quando criar a page de notifications, como usar a baixo:
+        //router.push('/notifications');
+    };
+
+    return (
+        // SafeAreaView serve para que nada fique em cima da barra de status do iOS
+        <SafeAreaView className="flex-1 bg-medix-50">
+            
+            <View className="flex-1 bg-medix-100"> {/* manter essa cor de fundo, para destacar o header */}
+                <Header 
+                    title="Home"
+                    onPressLeft={handleOpenDrawer}
+                    onPressRight={handleNotifications}
+                />
+
+                
+                
+                <View className="flex-1 items-center justify-center px-6 gap-8"> {/* FLEX 1 sempre */}
+                    <Text className="text-4xl font-bold text-medix-600 mb-2">👋 Olá!</Text>
+                    <Text className="text-lg text-medix-700 mb-1">Você está logado com sucesso.</Text>
+                    <Text className="text-sm text-medix-800 mb-8">Aproveite sua experiência no app 💚</Text>
+
+                    <Link replace href="/" className="bg-medix-600 px-6 py-3 rounded-lg">
+                        <Text className="text-white text-base font-semibold">Sair</Text>
+                    </Link>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 };
 
