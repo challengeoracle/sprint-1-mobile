dcda97bdb32c0a55afa1bc83cb7a9bd7e62953e4
Mateus Lima <Mateus Lima>
2025-09-30 21:10:09 -0300

    feat: agora o componente header de cada página é passado por aqui, o status bar foi atualizado para ficar no estilo dark assim não fica 'invisível' por causa da cor clara, para esconder o header de uma tela é só modificar o headerShown: false

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,44 @@
-import { Stack } from "expo-router";
-
+import Header from "@/components/Header";
+import { Stack, useRouter } from "expo-router";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 
 const RootLayout = () => {
+    const router = useRouter();
+
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-        </Stack>
+        <SafeAreaProvider>
+            <StatusBar barStyle="dark-content" backgroundColor="#F4F9F9" />
+            <Stack>
+                {/* Tela de login (index) */}
+                <Stack.Screen
+                    name="index"
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+
+                {/* Tela de cadastro de paciente (cadastro) */}
+                <Stack.Screen
+                    name="cadastro"
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Cadastro" leftIconName="arrow-left" onPressLeft={() => router.back()} />, // Usar o router back pra voltar
+                    }}
+                />
+
+                {/* Tela inicial (home) */}
+                <Stack.Screen
+                    name="home"
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Home" leftIconName="menu" onPressLeft={() => console.log("Ação para abrir o menu lateral")} rightIconName="bell" onPressRight={() => console.log("Navegar para a tela de notificações")} />,
+                    }}
+                />
+            </Stack>
+        </SafeAreaProvider>
     );
 };
+
 export default RootLayout;
